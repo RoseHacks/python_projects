0c886ba42937d6470b0ac5094eb5706761089590
Chandler <rk120169@gmail.com>
2026-01-07 15:47:50 -0500

    Platformer updates and restructures.

--- games/platformer/main.py
+++ games/platformer/main.py
@@ -1,1 +1,43 @@
-import pygame
+import pygame as pg
+import sys
+
+# Initializing pygame
+pg.init()
+
+class Game():
+
+    def __init__(self):
+        # Defining our window resolution - A surface we can draw onto.
+        self.screen = pg.display.set_mode((1280, 720))
+        # Force the game to run at 60 FPS. Our clock object. In a game, each frame is an interation in a loop
+        self.clock = pg.time.Clock()
+        # A pissed off hacker in black hoodie
+        # Takes place in cyberpunk world. Fighting blue team.
+        self.game_name = pg.display.set_caption("Attack of The Hacker")   
+        # image load function to put an image on the screen. Use PNG because it's lostless
+        self.img = pg.image.load('data/images/clouds/cloud_1.png')
+
+
+    def run(self):
+        while True:
+            # Despite math, top left is 0, 0 for non-gpu-based rendering techniques
+            self.screen.blit(self.img, (100, 200))
+            # In SDL, you have full control over the input handling
+            # Polling for events with pg.event.get() 
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()  # pygame exit
+                    sys.exit() # graceful system exit
+
+            # Update the display
+            pg.display.update()
+            self.clock.tick(60)
+
+game = Game()
+
+game.run()
+
+
+
+
+
--- userful_scripts/stay_awake.py
+++ userful_scripts/stay_awake.py
@@ -1,9 +1,13 @@
 import ctypes
 import time
 
+# This Python script uses the Windows API via the ctypes library to prevent the system from entering sleep
+
 ES_CONTINUOUS = 0x80000000
 ES_SYSTEM_REQUIRED = 0x00000001
 ES_DISPLAY_REQUIRED = 0x00000002
+
+# It epeatedly sets the thread execution state flags above ^, which signals to the system that user activity is ongoing.
 
 def keep_awake():
     ctypes.windll.kernel32.SetThreadExecutionState(
@@ -12,6 +16,8 @@ def keep_awake():
 
 def allow_sleep():
     ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS)
+
+# API call every 10 seconds to maintain this state.
 
 try:
     print("BRB...")
